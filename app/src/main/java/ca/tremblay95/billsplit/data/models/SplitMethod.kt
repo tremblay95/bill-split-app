0bd32684d4af865d53a8fa7c859cabd2a14eef71
marct <marc.tremblay1995@gmail.com>
2025-09-23 20:37:42 -0400

    pulled data/model changes into main branch

--- app/src/main/java/ca/tremblay95/billsplit/data/models/SplitMethod.kt
+++ app/src/main/java/ca/tremblay95/billsplit/data/models/SplitMethod.kt
@@ -2,12 +2,25 @@ package ca.tremblay95.billsplit.data.models
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "split_method",
-    indices = [Index(value = ["name"], unique = true)]
+    indices = [
+        Index(value = ["name"], unique = true),
+        Index(value = ["operation_id"])
+    ],
+    foreignKeys = [
+        ForeignKey(
+            entity = SplitOperation::class,
+            parentColumns = arrayOf("operation_id"),
+            childColumns = arrayOf("operation_id"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
 )
 data class SplitMethod(
     @PrimaryKey(autoGenerate = true)
@@ -18,5 +31,8 @@ data class SplitMethod(
     val name : String,
 
     @ColumnInfo("description")
-    val description : String
+    val description : String,
+
+    @ColumnInfo("operation_id")
+    val operationId : Int? = null
 )
--- app/src/main/java/ca/tremblay95/billsplit/data/models/SplitOperation.kt
+++ app/src/main/java/ca/tremblay95/billsplit/data/models/SplitOperation.kt
@@ -3,19 +3,14 @@ package ca.tremblay95.billsplit.data.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import ca.tremblay95.billsplit.ui.model.SplitType
 
 @Entity(
     tableName = "split_operation",
+    indices = [Index(value = ["parent_id"])],
     foreignKeys = [
-        ForeignKey(
-            entity = SplitMethod::class,
-            parentColumns = arrayOf("method_id"),
-            childColumns = arrayOf("method_id"),
-            onUpdate = ForeignKey.CASCADE,
-            onDelete = ForeignKey.CASCADE
-        ),
         ForeignKey(
             entity = SplitOperation::class,
             parentColumns = arrayOf("operation_id"),
@@ -30,12 +25,12 @@ data class SplitOperation(
     @ColumnInfo("operation_id")
     val operationId : Int = 0,
 
-    @ColumnInfo("method_id")
-    val methodId : Int,
-
     @ColumnInfo("parent_id")
     val parentId : Int?,
 
     @ColumnInfo("split_type")
     val splitType : SplitType,
+
+    @ColumnInfo("parent_operation_index")
+    val parentOperationIndex : Int?
 )
