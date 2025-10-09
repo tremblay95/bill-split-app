0ac7be429c7d0fd1c77d79d466a6a0f5f55091d2
marct <marc.tremblay1995@gmail.com>
2025-10-09 00:32:53 -0400

    OfflineSplitRepositoryTests getAllSplits tests

--- app/src/test/java/ca/tremblay95/billsplit/data/repository/OfflineSplitRepositoryTests.kt
+++ app/src/test/java/ca/tremblay95/billsplit/data/repository/OfflineSplitRepositoryTests.kt
@@ -5,6 +5,8 @@ import ca.tremblay95.billsplit.common.Result
 import ca.tremblay95.billsplit.data.fakes.FakeOperandDao
 import ca.tremblay95.billsplit.data.fakes.FakeOperationDao
 import ca.tremblay95.billsplit.data.fakes.FakeSplitDao
+import ca.tremblay95.billsplit.data.mappers.toSplit
+import ca.tremblay95.billsplit.data.model.SplitEntity
 import ca.tremblay95.billsplit.domain.model.Split
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.test.runTest
@@ -37,6 +39,37 @@ class OfflineSplitRepositoryTests {
             val actualSuccess = awaitItem()
             if (actualSuccess is Result.Success<List<Split>>) {
                 assertThat(actualSuccess.data).isEmpty()
+            }
+            else {
+                fail("Expected Result.Success")
+            }
+
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun getAllSplits_nonEmptyList_returnsResultLoading_completeList_ResultSuccess() = runTest {
+        val splitEntities = listOf(
+            SplitEntity(1, "1", "1"),
+            SplitEntity(2, "2", "2"),
+            SplitEntity(3, "3", "3")
+        )
+        splitEntities.forEach { splitDao.insertSplit(it) }
+
+        offlineSplitRepository.getAllSplits().test {
+            val actualLoading = awaitItem()
+            assertThat(actualLoading is Result.Loading).isTrue()
+
+            val actualSuccess = awaitItem()
+            if (actualSuccess is Result.Success<List<Split>>) {
+                assertThat(actualSuccess.data.size).isEqualTo(splitEntities.size)
+                
+                actualSuccess.data
+                    .zip(splitEntities)
+                    .forEach { (split, entity) ->
+                        assertThat(split).isEqualTo(entity.toSplit())
+                    }
             }
             else {
                 fail("Expected Result.Success")
